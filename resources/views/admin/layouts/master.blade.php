6f08b13ae7a0cdeba6e296ee2fce59914fc73baf
jatinder <development.geektech@gmail.com>
2022-08-05 17:14:48 +0530

    Dashboard setting

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -10,11 +10,11 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Admin Dashboard</title>
 	<!-- Custom fonts for this template-->
-	<link href="{{URL::to('/admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{URL::to('/admin-panel/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 	<!-- Custom styles for this template-->
-	<link href="{{URL::to('/admin/css/sb-admin-2.min.css')}}" rel="stylesheet"> </head>
-	<link href="{{URL::to('/admin/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+	<link href="{{URL::to('/admin-panel/css/sb-admin-2.min.css')}}" rel="stylesheet"> </head>
+	<link href="{{URL::to('/admin-panel/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 	
 
 <body id="page-top">
